7dedc5076a3018ae6ecf3278b7ce8dee410c0fd7
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-04-19 18:12:28 +0700

    install cookie-parser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const auth = require('./middlewares/auth');
 const { signin, signup, logout } = require('./controllers/user');
 const NotFoundError = require('./errors/NotFoundError');
@@ -14,9 +15,11 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(cookieParser());
+
 app.post('/signin', signin);
 app.post('/signup', signup);
-app.get('/logout', logout);
+app.get('/logout', auth, logout);
 
 app.use('/', auth, require('./routes/users'));
 app.use('/', auth, require('./routes/movies'));
