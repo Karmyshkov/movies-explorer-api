48c30f964847eff96a124359c3c4701679ac33bd
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-04-19 18:12:44 +0700

    refactoring code

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,7 +2,6 @@
 const errorHandler = (err, req, res, next) => {
   const statusCode = err.status || 500;
   const message = statusCode === 500 ? 'На сервере произошла ошибка' : err.message;
-
   res.status(statusCode).send({ message, err });
 };
 
