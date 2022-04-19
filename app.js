59d3cd77190f2b7c9e20c2a595b243e12494111b
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-04-19 16:28:17 +0700

    added routes in app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const { signin, signup } = require('./controllers/user');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Test');
-});
+app.post('/signin', signin);
+app.post('/signup', signup);
+
+app.use('/', require('./routes/users'));
+app.use('/', require('./routes/movies'));
 
 app.listen(3000);
