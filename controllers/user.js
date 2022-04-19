f238cbb143a6dd6a83584bdf3373031e07a6b654
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-04-19 16:58:55 +0700

    added logic for signin controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,45 @@
+require('dotenv').config();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const ConflictError = require('../errors/ConflictError');
 const BadRequestError = require('../errors/BadRequestError');
+const UnauthorizedError = require('../errors/UnauthorizedError');
+
+const { NODE_ENV, SECRET_KEY } = process.env;
 
 const signin = (req, res, next) => {
-  res.send('signin');
+  const { email, password } = req.body;
+
+  User.findOne({ email }, '+password').then((user) => {
+    if (!user) {
+      throw new UnauthorizedError();
+    }
+    return bcrypt.compare(password, user.password).then((matched) => {
+      if (!matched) {
+        throw new UnauthorizedError();
+      }
+      return user;
+    });
+  })
+    .then((user) => {
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? SECRET_KEY : 'SECRET_KEY_TEST',
+        {
+          expiresIn: '7d',
+        },
+      );
+      res
+        .cookie('jwt', token, {
+          maxAge: 3600000 * 24 * 7,
+          httpOnly: true,
+          sameSite: true,
+        })
+        .status(200)
+        .send({ message: 'Успешная авторизация' });
+    })
+    .catch(next);
 };
 
 const signup = (req, res, next) => {
